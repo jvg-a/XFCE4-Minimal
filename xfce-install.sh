324b22ca9723461fb095a08bb6cc2e59665193a1
JVGA <57042644+jvg-a@users.noreply.github.com>
2025-03-29 19:10:55 -0300

    xfce-install.sh

--- xfce-install.sh
+++ xfce-install.sh
@@ -11,21 +11,8 @@ fi
 cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
 chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
 
-apt install -y \
-    libxfce4ui-utils \
-    thunar \
-    xfce4-appfinder \
-    xfce4-panel \
-    xfce4-pulseaudio-plugin \
-    xfce4-whiskermenu-plugin \
-    xfce4-session \
-    xfce4-settings \
-    xfce4-terminal \
-    xfconf \
-    xfdesktop4 \
-    xfwm4 \
-    adwaita-qt \
-    qt5ct 
+apt install -y libxfce4ui-utils thunar xfce4-appfinder xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xfce4-session xfce4-settings xfce4-terminal xfce4-goodies xfconf xfdesktop4 xfwm4 adwaita-qt qt5ct network-manager-openvpn network-manager-gnome network-manager-openvpn-gnome
+
 
 echo 
 echo xfce install complete, please reboot and issue 'startx'
