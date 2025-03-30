04dbb878491937f8daed3d074927bc63fbd8e7b2
JVGA <57042644+jvg-a@users.noreply.github.com>
2025-03-29 23:58:10 -0300

    Update xfce-install.sh

--- xfce-install.sh
+++ xfce-install.sh
@@ -4,15 +4,21 @@
 
 ## check for sudo/root
 if ! [ $(id -u) = 0 ]; then
-  echo "This script must run with sudo, try again..."
+  echo "You must run with root"
   exit 1
 fi
 
-cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
-chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
+apt install sudo -y
 
 apt install -y libxfce4ui-utils thunar xfce4-appfinder xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xfce4-session xfce4-settings xfce4-terminal xfce4-goodies xfconf xfdesktop4 xfwm4 adwaita-qt qt5ct network-manager-openvpn network-manager-gnome network-manager-openvpn-gnome
 
+unzip theme.zip
+cd theme
+cp -r theme/xfce4 /etc/xdg/
+cp -r theme/themes /usr/share/
+cp -r theme/icons /usr/share/
+
+cd
 
 echo 
 echo xfce install complete, please reboot and issue 'startx'
